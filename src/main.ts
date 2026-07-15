ee444dbb06e99b62b67fe1d2cbea6e103e41d6c7
nrttns <nrttns@hotmail.com>
2026-07-15 11:21:54 +0300

    Add testable CLI runner

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 import { getBootMessage } from './boot';
 
-console.log(getBootMessage());
+export type WriteLine = (message: string) => void;
+
+export function runCli(writeLine: WriteLine = console.log): string {
+  const message = getBootMessage();
+  writeLine(message);
+  return message;
+}
+
+if (require.main === module) {
+  runCli();
+}
