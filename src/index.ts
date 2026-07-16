070399290cab14654575bfb466fe556e058f4c2c
nrttns <nrttns@hotmail.com>
2026-07-16 19:01:15 +0300

    Export Target contract loader

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { bootMessage, getBootMessage, getBootStatus } from './boot';
-export { formatBootOutput, getCliStatus, parseCliArgs, runCli } from './main';
+export { formatBootOutput, getCliStatus, loadTargetContract, parseCliArgs, runCli } from './main';
 export type { BootStatus } from './boot';
-export type { CliStatus, OutputFormat, RunCliOptions, WriteLine } from './main';
+export type { CliStatus, OutputFormat, RunCliOptions, TargetContract, WriteLine } from './main';
